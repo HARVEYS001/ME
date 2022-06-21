6bd88b31586a9a1475ad36ba9fb9412d73540332
HARVEYS001 <107902679+HARVEYS001@users.noreply.github.com>
2022-06-21 12:46:16 +0930

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+music.playTone(440, music.beat(BeatFraction.Breve))
+basic.forever(function () {
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+})
